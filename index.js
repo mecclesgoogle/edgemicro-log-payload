824d39acb0fbbbfe6ba03d9ab3f0647e7bba7b02
Mark Eccles <44128199+mecclesgoogle@users.noreply.github.com>
2021-03-02 13:48:41 +1100

    Update index.js
    Fix #1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 var os = require('os');
 
+const LOG_TAG_COMP = 'log-payload';
+
 module.exports.init = function(config, logger, stats) {
 
   const formatHeaders = (headers) => {
@@ -19,20 +21,30 @@ module.exports.init = function(config, logger, stats) {
   return {
 
     onend_request: function(req, res, data, next) {
-      logger.info(`Request Headers: ${formatHeaders(req.headers)}`);
-      logger.info(`Request payload:${os.EOL}${data}`);
+      logger.eventLog(
+        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        `Request Headers: ${formatHeaders(req.headers)}`);
+      logger.eventLog(
+        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        `Request payload:${os.EOL}${data}`);
       next(null, data);
     },
 
     onend_response: function(req, res, data, next) {
-      logger.info(`Response Headers: ${formatHeaders(res.getHeaders())}`);
-      logger.info(`Response payload:${os.EOL}${data}`);
+      logger.eventLog(
+        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+       `Response Headers: ${formatHeaders(res.getHeaders())}`);
+      logger.eventLog(
+        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        `Response payload:${os.EOL}${data}`);
       next(null, data);
     },
 
     onerror_response: function(req, res, err, next) {
-      logger.info(`Response Headers: ${formatHeaders(res.getHeaders())}`);
-      logger.info(`Response payload:${os.EOL}${data}`);
+      logger.eventLog(
+        {level:'info', req: req, res: res, err:err, component:LOG_TAG_COMP },
+        `Response Headers: ${formatHeaders(res.getHeaders())}`
+      );
       next();
     }
    
