e560e2a2be824a9b3ac9440867575a8bf92f4635
Mark Eccles <44128199+mecclesgoogle@users.noreply.github.com>
2021-03-02 13:49:21 +1100

    Update index.js
    formatting

--- index.js
+++ index.js
@@ -22,27 +22,27 @@ module.exports.init = function(config, logger, stats) {
 
     onend_request: function(req, res, data, next) {
       logger.eventLog(
-        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        {level:'info', req: req, res: res, err: null, component:LOG_TAG_COMP }, 
         `Request Headers: ${formatHeaders(req.headers)}`);
       logger.eventLog(
-        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        {level:'info', req: req, res: res, err: null, component:LOG_TAG_COMP }, 
         `Request payload:${os.EOL}${data}`);
       next(null, data);
     },
 
     onend_response: function(req, res, data, next) {
       logger.eventLog(
-        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        {level:'info', req: req, res: res, err: null, component:LOG_TAG_COMP }, 
        `Response Headers: ${formatHeaders(res.getHeaders())}`);
       logger.eventLog(
-        {level:'info', req: req, res: res, err:null, component:LOG_TAG_COMP }, 
+        {level:'info', req: req, res: res, err: null, component:LOG_TAG_COMP }, 
         `Response payload:${os.EOL}${data}`);
       next(null, data);
     },
 
     onerror_response: function(req, res, err, next) {
       logger.eventLog(
-        {level:'info', req: req, res: res, err:err, component:LOG_TAG_COMP },
+        {level:'info', req: req, res: res, err: err, component:LOG_TAG_COMP },
         `Response Headers: ${formatHeaders(res.getHeaders())}`
       );
       next();
